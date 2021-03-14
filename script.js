695200580664c6dd4a47e4983a405150fc531366
Yulia Kozlova <ykozlova3103@yandex.ru>
2021-03-14 17:50:43 +0300

    1 версия

---
+++ script.js
@@ -0,0 +1,40 @@
+
+let popup = document.querySelector('.popup');
+let openPopupButton = document.querySelector('.profile__buttom-pencil');
+let openPopupButtonPencil = document.querySelector('.profile__pencil');
+let closePopupButton = document.querySelector('.popup__closebutton');
+let popupOverlay = document.querySelector('.popup__overlay');
+
+function openPopup() {
+  popup.classList.add('popup__visible');
+}
+function closePopup() {
+  popup.classList.remove('popup__visible');
+}
+openPopupButton.addEventListener('click', function() {
+  openPopup();
+});
+openPopupButtonPencil.addEventListener('click', function() {
+  openPopup();
+});
+closePopupButton.addEventListener('click', function(){
+  closePopup();
+});
+popupOverlay.addEventListener('click', function(){
+  closePopup();
+});
+
+let formElement = document.querySelector('.popup__content');
+let nameInput = formElement.querySelector('.popup__name');
+let jobInput = formElement.querySelector('.popup__job');
+let buttomSave = formElement.querySelector('.popup__button_save');
+let profileName = document.querySelector('.profile__name');
+let profileProfession = document.querySelector('.profile__profession');
+
+function formSubmitHandler (evt) {
+  evt.preventDefault();
+  profileName.textContent = nameInput.value;
+  profileProfession.textContent = jobInput.value;
+  closePopup();
+}
+formElement.addEventListener('submit', formSubmitHandler);
