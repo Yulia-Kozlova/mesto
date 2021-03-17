e340a9103fb9e355a85097c78e394551ad48efc4
Yulia Kozlova <ykozlova3103@yandex.ru>
2021-03-17 22:40:04 +0300

    исправленная работа после замечания ревьюера

--- script.js
+++ script.js
@@ -1,41 +1,31 @@
 
 let popup = document.querySelector('.popup');
-let openPopupButton = document.querySelector('.profile__buttom-pencil');
-let openPopupButtonPencil = document.querySelector('.profile__pencil');
-let closePopupButton = document.querySelector('.popup__closebutton');
-let popupOverlay = document.querySelector('.popup__overlay');
-
-function openPopup() {
-  popup.classList.add('popup__visible');
-}
-function closePopup() {
-  popup.classList.remove('popup__visible');
-}
-openPopupButton.addEventListener('click', function() {
-  openPopup();
-});
-openPopupButtonPencil.addEventListener('click', function() {
-  openPopup();
-});481
-5*
-closePopupButton.addEventListener('click', function(){
-  closePopup();
-});
-popupOverlay.addEventListener('click', function(){
-  closePopup();
-});
-
-let formElement = document.querySelector('.popup__content');
-let nameInput = formElement.querySelector('.popup__name');
-let jobInput = formElement.querySelector('.popup__job');
-let buttomSave = formElement.querySelector('.popup__save');
+let openPopupButton = document.querySelector('.profile__button-pencil');
+let closePopupButton = popup.querySelector('.popup__closebutton');
+let popupOverlay = popup.querySelector('.popup__overlay');
+let formElement = popup.querySelector('.popup__content');
+let nameInput = popup.querySelector('.popup__name');
+let jobInput = popup.querySelector('.popup__job');
+let buttomSave = popup.querySelector('.popup__save');
 let profileName = document.querySelector('.profile__name');
 let profileProfession = document.querySelector('.profile__profession');
 
+function openPopup() {
+  popup.classList.add('popup_opened');
+  nameInput.value = profileName.textContent;
+  jobInput.value = profileProfession.textContent;
+}
+function closePopup() {
+  popup.classList.remove('popup_opened');
+}
 function formSubmitHandler (evt) {
   evt.preventDefault();
   profileName.textContent = nameInput.value;
   profileProfession.textContent = jobInput.value;
   closePopup();
 }
+
+openPopupButton.addEventListener('click', openPopup);
+closePopupButton.addEventListener('click', closePopup);
+popupOverlay.addEventListener('click', closePopup);
 formElement.addEventListener('submit', formSubmitHandler);
